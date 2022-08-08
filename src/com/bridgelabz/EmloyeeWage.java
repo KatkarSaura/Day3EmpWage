bff0b0e20d6c0d9e8544af605b87498026331bb2
Pooja <pooja.katkr@gmail.com>
2022-08-08 23:31:17 +0530

    Uc2pr added

--- src/com/bridgelabz/EmloyeeWage.java
+++ src/com/bridgelabz/EmloyeeWage.java
@@ -13,5 +13,20 @@ public class EmloyeeWage {
 			else
 		       System.out.println("Employee is Abscent");	
 			
+			IS_FULL_TIME = 1;
+			int EMP_RATE_PER_HOUR = 20;
+			int empHrs = 0;
+			int empWage = 0;
+			empCheck = Math.floor(Math.random() * 10) %2;
+			if (empCheck == IS_FULL_TIME)
+				empHrs = 8;
+			else 
+				empHrs = 0;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("emp Wage:" + empWage);
+
+		
+
+			
 	}
 }
