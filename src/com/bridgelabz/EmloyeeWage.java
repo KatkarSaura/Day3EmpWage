c6bcfc7673b28fae2e8cc233da364a929ece934e
Pooja <pooja.katkr@gmail.com>
2022-08-08 23:19:18 +0530

    employee wage random no

--- src/com/bridgelabz/EmloyeeWage.java
+++ src/com/bridgelabz/EmloyeeWage.java
@@ -11,7 +11,7 @@ public class EmloyeeWage {
 			if (empCheck == IS_FULL_TIME)
 			   System.out.println("Employee is Present");
 			else
-		       System.out.println("Employee is Abscent");		
-			}
+		       System.out.println("Employee is Abscent");	
+			
 	}
-
+}
